f178e3408e78f751d1a154b337bcfa87e6dfcf7f
Majid <Mansouridb@gmail.com>
2021-04-21 14:56:20 +0200

    Initial commit

---
+++ src/test/java/org/example/AfterTrim.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class AfterTrim {
+    public static void main(String[] args) {
+        String str= new String("\tJ\ta\tv\ta\t");
+        System.out.println(str.trim());
+
+    }
+}
